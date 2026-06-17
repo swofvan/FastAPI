8e0b2a37097f0e526e6b5ab7a3d9e0f591ebdb96
Mohammed Swofvan P <swofvanmhd@gmail.com>
2026-06-17 20:46:14 +0530

    get and post method

--- first_project/main.py
+++ first_project/main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")      # path
 def home():
     return {"message" : "Hello World"}
+
+@app.get("/about")      # path
+def about():
+    return {"message" : "Aboutus"}
+
+
+products = []   # Temporary storage
+
+class Product(BaseModel):   # model
+    name: str
+    price: float
+
+@app.post("/products")
+def create_product(product:Product):
+    products.append(product)
+    return {
+        "message":"Product created successfully",
+        "data": product
+        }
+
+
+@app.get("/products")
+def get_prodects():
+    return products
