e8406b8f430186a5eb8f11647abfe42fa346d5d6
Mohammed Swofvan P <swofvanmhd@gmail.com>
2026-06-23 14:56:02 +0530

    database added

---
+++ first_project/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql+mysqldb://root:@localhost:3306/dummy_shop"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit = False,
+    autoflush = False,
+    bind = engine
+)
+
--- first_project/main.py
+++ first_project/main.py
@@ -1,32 +1,42 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+# from pydantic import BaseModel
+
+# app = FastAPI()
+
+# @app.get("/")      # path
+# def home():
+#     return {"message" : "Hello World"}
+
+# @app.get("/about")      # path
+# def about():
+#     return {"message" : "Aboutus"}
+
+
+# products = []   # Temporary storage
+
+# class Product(BaseModel):   # model
+#     name: str
+#     price: float
+
+# @app.post("/products")
+# def create_product(product:Product):
+#     products.append(product)
+#     return {
+#         "message":"Product created successfully",
+#         "data": product
+#         }
+
+
+# @app.get("/products")
+# def get_prodects():
+#     return products
+
+from database import engine
 
 app = FastAPI()
 
-@app.get("/")      # path
+@app.get("/")
 def home():
-    return {"message" : "Hello World"}
+    return {"message": "Database connected successfully"}
 
-@app.get("/about")      # path
-def about():
-    return {"message" : "Aboutus"}
-
-
-products = []   # Temporary storage
-
-class Product(BaseModel):   # model
-    name: str
-    price: float
-
-@app.post("/products")
-def create_product(product:Product):
-    products.append(product)
-    return {
-        "message":"Product created successfully",
-        "data": product
-        }
-
-
-@app.get("/products")
-def get_prodects():
-    return products
