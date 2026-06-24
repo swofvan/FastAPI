c029da16820f22fa60052c9052154a512775c78c
Mohammed Swofvan P <swofvanmhd@gmail.com>
2026-06-24 11:57:48 +0530

    database edited

--- first_project/database.py
+++ first_project/database.py
@@ -1,13 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine        # Create a connection setup for the database.
+from sqlalchemy.orm import sessionmaker     # A Session is like a temporary workspace for database operations.
 
-DATABASE_URL = "mysql+mysqldb://root:@localhost:3306/dummy_shop"
+DATABASE_URL = "mysql+mysqldb://root:@localhost:3306/dummy_shop"   # database address.
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL)   # SQLAlchemy creates an Engine object
 
 SessionLocal = sessionmaker(
-    autocommit = False,
-    autoflush = False,
-    bind = engine
+    autocommit = False,         # Without committing, MySQL doesn't save it permanently.
+    autoflush = False,          # Send changes to the database temporarily. false means not tremp saves
+    bind = engine               # Attach this Session Factory to this Engine
 )
 
