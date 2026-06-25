784510affd0e5c80db98f6c74177e21e9eb33079
Mohammed Swofvan P <swofvanmhd@gmail.com>
2026-06-25 21:26:40 +0530

    models created

---
+++ first_project/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Float
+
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()  # Every SQLAlchemy model must inherit from Base.
+
+class Products(Base):
+    __tablename__ = "products"   # Create a table named products
+
+    id = Column(Integer, primary_key=True, index=True)
+    product_name = Column(String(100))
+    category = Column(String(100))
+    price = Column(Float)
+    quantity = Column(Integer)
+
